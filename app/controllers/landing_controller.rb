89d0c36146dee5f9b85bb97023b2826db56ce757
Feinw <filbertwee@yahoo.com>
2020-04-04 20:55:48 +0800

    Add logarithmic scale

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -7,7 +7,6 @@ class LandingController < ApplicationController
     @trend = getCumulativeTrend
     @c_dist = getCovidDist
     @p_dist = getPUIDist
-    @annotations_array = [1,2,3,4,5,6,7,8,9]
   end
 
   private
